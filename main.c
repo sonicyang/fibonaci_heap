c7dc3f602d518f32bb528ac77a3cb809cd3a2c39
sonicyang <sonic.tw.tp@gmail.com>
2016-05-29 02:04:29 +0800

    Add Example usage

--- main.c
+++ main.c
@@ -1,81 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "table.h"
+#include "fb.h"
 
-void print_table(struct int_table_t* table){
+void print_node(struct node_t* n, int x){
+    struct node_t* end;
     int i;
-    for(i = 0; i < table->size; i++){
-        if(table->used[i]){
-            printf("%d ", table->data[i]);
-        }else{
-            printf("x ");
-        }
+
+    if(n != NULL){
+        end = n;
+        do{
+            printf("(%d", n->key);
+            if(n->degree)
+                print_node(n->child, 1);
+            printf(")");
+            if(!x)
+                printf("\n");
+            n = n->right;
+        }while(n != end);
     }
-    printf("\n");
+
+}
+
+void print_fib_heap(struct fib_heap_t* h){
+    print_node(h->min, 0);
 }
 
 int main(int argc, char* argv[]){
-    struct int_table_t* table;
+    struct fib_heap_t* h;
 
-    //Init a Table
-    puts("Table created");
-    table = create_int_table();
+    //Init a Fib Heap
+    puts("Fib Heap created");
+    h = make_fib_heap();
 
-    //Insert 10 numbers, track the data
-    puts("Instert 10 numbers");
-    int_table_insert(table, 1);
-    print_table(table);
+    puts("Insert 10 elements");
+    fib_heap_insert(h, 10);
+    fib_heap_insert(h, 20);
+    fib_heap_insert(h, 30);
+    fib_heap_insert(h, 40);
+    print_fib_heap(h);
 
-    int_table_insert(table, 4);
-    print_table(table);
+    printf("Extract min: %d\n", fib_heap_extract_min(h));
 
-    int_table_insert(table, 2);
-    print_table(table);
+    puts("After extract min");
+    print_fib_heap(h);
 
-    int_table_insert(table, 7);
-    print_table(table);
-
-    int_table_insert(table, 9);
-    print_table(table);
-
-    int_table_insert(table, 10);
-    print_table(table);
-
-    int_table_insert(table, 3);
-    print_table(table);
-
-    int_table_insert(table, 5);
-    print_table(table);
-
-    int_table_insert(table, 6);
-    print_table(table);
-
-    int_table_insert(table, 8);
-    print_table(table);
-
-    //Delete 6 elements by index
-    puts("Remove 6 elements");
-    int_table_delete(table, 0);
-    print_table(table);
-
-    int_table_delete(table, 2);
-    print_table(table);
-
-    int_table_delete(table, 4);
-    print_table(table);
-
-    int_table_delete(table, 6);
-    print_table(table);
-
-    int_table_delete(table, 8);
-    print_table(table);
-
-    int_table_delete(table, 1);
-    print_table(table);
-
-    //free the table
-    puts("Freeing the table");
-    delete_int_table(table);
+    //free the heap
+    puts("Freeing the fib heap");
+    delete_fib_heap(h);
     return 0;
 }
 
